07b0e2c035bf7986075ad7ac8c8396d74a5be4f2
N○VA STUDI●S <elgatovolador3500@gmail.com>
2026-06-07 08:38:49 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -38,33 +38,82 @@ export class Game{
         });
     }
 
-    update(){
+   update(){
 
-        let input={
-            x:0,
-            y:0
-        };
+    let input={
+        x:0,
+        y:0
+    };
 
-        if(this.keys["a"]) input.x=-1;
-        if(this.keys["d"]) input.x=1;
-        if(this.keys["w"]) input.y=-1;
-        if(this.keys["s"]) input.y=1;
+    let attack = false;
 
-        const gamepad=getGamepadInput();
+    if(this.keys["a"]) input.x=-1;
+    if(this.keys["d"]) input.x=1;
+    if(this.keys["w"]) input.y=-1;
+    if(this.keys["s"]) input.y=1;
 
-        if(gamepad.x!==0)
-            input.x=gamepad.x;
-
-        if(gamepad.y!==0)
-            input.y=gamepad.y;
-
-        this.player.update(input);
-
-        this.enemies.forEach(e=>
-            e.update(this.player)
-        );
+    if(this.keys[" "]){
+        attack = true;
     }
 
+    const gamepad = getGamepadInput();
+
+    if(gamepad.attack){
+        attack = true;
+    }
+
+    if(gamepad.x !== 0)
+        input.x = gamepad.x;
+
+    if(gamepad.y !== 0)
+        input.y = gamepad.y;
+
+    this.player.update(input);
+
+    if(
+        attack &&
+        this.player.attackCooldown <= 0
+    ){
+
+        this.player.attacking = true;
+
+        this.player.attackCooldown = 20;
+
+        this.enemies.forEach(enemy=>{
+
+            const dx =
+            enemy.x - this.player.x;
+
+            const dy =
+            enemy.y - this.player.y;
+
+            const dist =
+            Math.hypot(dx,dy);
+
+            if(dist < 120){
+
+                enemy.hp -= this.player.damage;
+
+            }
+
+        });
+
+    }else{
+
+        this.player.attacking = false;
+
+    }
+
+    this.enemies.forEach(enemy=>
+        enemy.update(this.player)
+    );
+
+    this.enemies =
+    this.enemies.filter(
+        enemy => enemy.hp > 0
+    );
+
+}
     draw(){
 
         const ctx=this.ctx;
